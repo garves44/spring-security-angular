546cc7cb4c74317e228badba6f6d4041d8ebe281
Jeremy Collins <jeremycollins@utah.gov>
2020-12-09 12:23:31 -0700

    fixed compiling bugs recreated main app

--- src/main/java/com/example/springsecurityangular/SpringSecurityAngularApplication.java
+++ src/main/java/com/example/springsecurityangular/SpringSecurityAngularApplication.java
@@ -1,7 +1,14 @@
 package com.example.springsecurityangular;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
@@ -9,11 +16,19 @@ public class SpringSecurityAngularApplication {
 
 	@GetMapping("/resource")
 	@ResponseBody
-	public Map<String, Object> main(String[] args) {
-		Map<String, Object> model = new HashMap<String, Object>();
-    	model.put("id", UUID.randomUUID().toString());
-    	model.put("content", "Hello World");
-    	return model;
+	public Map<String, Object> home() {
+	  Map<String, Object> model = new HashMap<String, Object>();
+	  model.put("id", UUID.randomUUID().toString());
+	  model.put("content", "Hello World");
+	  return model;
 	}
-
-}
+  
+	@GetMapping(value = "/{path:[^\\.]*}")
+	public String redirect() {
+	  return "forward:/";
+	}
+  
+	public static void main(String[] args) {
+	  SpringApplication.run(SpringSecurityAngularApplication.class, args);
+	}
+  }
