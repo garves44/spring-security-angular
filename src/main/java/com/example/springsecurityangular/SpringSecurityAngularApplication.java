fddd24bfc2d4efb594d040cbd282aafe479fe3a3
Jeremy Collins <jeremycollins@utah.gov>
2020-12-09 11:25:44 -0700

    setup Spring Boot for backend and Angular for frontend

--- src/main/java/com/example/springsecurityangular/SpringSecurityAngularApplication.java
+++ src/main/java/com/example/springsecurityangular/SpringSecurityAngularApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Controller
 public class SpringSecurityAngularApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityAngularApplication.class, args);
+	@GetMapping("/resource")
+	@ResponseBody
+	public Map<String, Object> main(String[] args) {
+		Map<String, Object> model = new HashMap<String, Object>();
+    	model.put("id", UUID.randomUUID().toString());
+    	model.put("content", "Hello World");
+    	return model;
 	}
 
 }
